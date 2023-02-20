ea4f1691e9d6d4051653b30ef02705373fadad23
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-02-20 23:40:04 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function handle(){
+        console.log('Good!')
+    }
+  return(
+    <div>
+        <button onFocus={handle} onBlur={(e)=> console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
+import { useState } from "react";
 
 function Keypad (){
+    const [password , setPassword] = useState(""); 
+    function handle(e){
+       setPassword(e.target.value);
+       console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" value={password} onChange={handle}/>
+        </div>
     )
 }
 
